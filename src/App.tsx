f380b2928401cfcdedcb7c337374fd1bfee098ad
SeeJia <seejiajia1010@gmail.com>
2023-06-26 11:38:55 +0800

    Proptypes by using typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import React from "react";
 import './App.css';
+import { Person } from "./components/Person";
 
 function App() {
   return (
     <div className="App">
+
+      <Person 
+      
+      name="Pedro"
+      email="Pedro@gmail.com"
+      age={21}
+      isMarried={true}
+      friends = {["james","marry"]}
+
+      />
       
     </div>
   );
