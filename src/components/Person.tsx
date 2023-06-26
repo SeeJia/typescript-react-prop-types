a1e3f21397ec9d5ee6b0ec57d6769c85566bcebb
SeeJia <seejiajia1010@gmail.com>
2023-06-26 11:44:15 +0800

    create the interface Props to declare all the variable

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,11 +1,20 @@
-export const Person = (props) => {
+interface Props {
+    name:string;
+    email:string;
+    age:number;
+    isMarried:Boolean;
+    friends:string[];
+}
+
+
+export const Person = (props : Props) => {
     return(
         <div>
             <h1>Name: {props.name}</h1>
             <h1>Email: {props.email}</h1>
             <h1>Age: {props.age}</h1>
             <h1>This person {props.isMarried ? "is":"is not"} Married</h1>
-            {props.friends.map((friend) =>(
+            {props.friends.map((friend : string) =>(
                  <h1>{friend}</h1> 
             ))}
         </div>
