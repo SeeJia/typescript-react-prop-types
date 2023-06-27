af32aee21c653ff56143438af7676c9bbeee2479
SeeJia <seejiajia1010@gmail.com>
2023-06-27 11:20:14 +0800

    getting login and logout function

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,22 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+const initialState = {value: {username: ""}}
+
 const userSlice = createSlice({
     name: "user",
-    initialState: {value: {username: ""}},
+    initialState,
     reducers:{
         login: (state, action) =>{
-            state.value = action.payload
+            state.value = action.payload;
+        },
+
+        logout:(state) =>{
+            state = initialState;
         }
     }
 });
+
+export const {} = userSlice.actions;
 
 export const store = configureStore({
 
