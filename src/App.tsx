de89db5a5513b5be1b531604bb3e3bfa5dc01f50
SeeJia <seejiajia1010@gmail.com>
2023-06-27 11:00:25 +0800

    add provider to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { Home } from './pages/Home';
 import { Contact } from './pages/Contact';
-import { Login } from './pages/login';
+import { Login } from './pages/Login';
+import { Provider } from "react-redux";
+import { store } from './store';
 
 function App() {
   return (
     <div className="App">
-    
+    <Provider store={store}> 
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
@@ -15,7 +17,7 @@ function App() {
         <Route path='/contact' element={<Contact />} />
       </Routes>
     </Router>
-      
+    </Provider>
     </div>
   );
 }
