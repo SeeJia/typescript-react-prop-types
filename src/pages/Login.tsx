4346bed0205a169273d721b4fabe49454786840b
SeeJia <seejiajia1010@gmail.com>
2023-06-27 11:39:43 +0800

    pass input data to username

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,18 @@
 import {useState} from "react";
+import { login, logout} from "../store";
+import {useDispatch} from "react-redux";
 
 export const Login = () =>{
     const [newUsername, setNewUsername] = useState("");
+    
+    const dispatch = useDispatch();
+    
     return(
     <h1>This is the login page.
 
     <input onChange={(e) =>{setNewUsername(e.target.value)}} />
 
-    <button>Submit Login</button>
+    <button onClick={() => dispatch(login({username: newUsername}))}>Submit Login</button>
     <button>Logout</button>
 
     </h1>
