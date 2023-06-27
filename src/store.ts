91491bd8c0062bf232b2b3e50e7d235c9111192b
SeeJia <seejiajia1010@gmail.com>
2023-06-27 11:13:20 +0800

    payload to pass data here

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,14 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-    
-})
+    name: "user",
+    initialState: {value: {username: ""}},
+    reducers:{
+        login: (state, action) =>{
+            state.value = action.payload
+        }
+    }
+});
 
 export const store = configureStore({
 
